a5f13dab80f115e57b5eb6200f774a9ae7c71b2a
victornaya-dev <valfonsonaya@gmail.com>
2026-06-07 00:08:00 +0200

    feat: CloudWatch alarms + SNS alerts

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,51 @@
+resource "aws_sns_topic" "alerts" {
+  name = "food-facts-alerts"
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "food-facts-lambda-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = "food-facts-transform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_duration" {
+  alarm_name          = "food-facts-lambda-duration"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 25000
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = "food-facts-transform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "kinesis_lag" {
+  alarm_name          = "food-facts-kinesis-lag"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "GetRecords.IteratorAgeMilliseconds"
+  namespace           = "AWS/Kinesis"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 60000
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    StreamName = "food-facts-stream"
+  }
+}
