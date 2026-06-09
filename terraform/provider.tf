a15943fb6ca43eef10577d927a4e1100c6cf3ab3
victornaya-dev <valfonsonaya@gmail.com>
2026-06-09 17:35:00 +0200

    fix: add S3 backend for terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,9 +5,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "food-facts-raw-victor"
+    key    = "terraform/state/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
