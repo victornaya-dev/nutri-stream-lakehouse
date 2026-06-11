f75af8b951f9aa473de50a9747a00a1c4ed234b4
victornaya-dev <valfonsonaya@gmail.com>
2026-06-11 16:24:19 +0200

    refactor: use variable for Glue output path

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -14,11 +14,11 @@ resource "aws_glue_crawler" "json_crawler" {
 }
 
 resource "aws_glue_crawler" "parquet_crawler" {
-  name = "food-facts-parquet-crawler"
+  name          = "food-facts-parquet-crawler"
   database_name = "food_facts_db"
   role          = aws_iam_role.glue_role.name
 
-    configuration = jsonencode({
+  configuration = jsonencode({
     Version              = 1.0
     CreatePartitionIndex = true
   })
@@ -35,13 +35,13 @@ resource "aws_glue_catalog_database" "food_facts" {
 
 
 resource "aws_glue_job" "etl" {
-  name             = "food-facts-etl"
-  role_arn         = aws_iam_role.glue_role.arn
-  glue_version     = "5.1"
-  worker_type      = "G.1X"
+  name              = "food-facts-etl"
+  role_arn          = aws_iam_role.glue_role.arn
+  glue_version      = "5.1"
+  worker_type       = "G.1X"
   number_of_workers = 10
-  timeout          = 480
-  execution_class  = "STANDARD"
+  timeout           = 480
+  execution_class   = "STANDARD"
 
   command {
     script_location = "s3://aws-glue-assets-${var.aws_account_id}-eu-west-1/scripts/food-facts-etl.py"
@@ -60,5 +60,6 @@ resource "aws_glue_job" "etl" {
     "--enable-spark-ui"                  = "true"
     "--job-bookmark-option"              = "job-bookmark-disable"
     "--spark-event-logs-path"            = "s3://aws-glue-assets-${var.aws_account_id}-eu-west-1/sparkHistoryLogs/"
+    "--output_path"                      = "s3://${var.bucket_processed}/parquet/"
   }
 }
