905e5c4301f3382907ffe04d52ec1cf8f043a0e7
victornaya-dev <valfonsonaya@gmail.com>
2026-06-24 20:59:19 +0200

    revert: restore S3 backend for production account

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "food-facts-raw-victor"
+    key    = "terraform/state/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
 
 provider "aws" {
